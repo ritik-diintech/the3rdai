d9dab2f5ea98e62101728f4a3e76019d1095f334
ritik_kumar7 <ritik.r9k@gmail.com>
2026-07-10 20:34:43 +0530

    Upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello World</h1>
+      <p>Welcome to The 3rd AI React App</p>
+    </div>
+  )
+}
+
+export default App
